b8375c207749028dbd43b8c2a4d7744e91a3cb9b
郑利涛 <zhenglitao@zhenglitaodeMacBook-Pro.local>
2019-01-27 10:55:43 +0800

    oled

--- SteeringModule/Rotation.py
+++ SteeringModule/Rotation.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 
 class Rotation:
-        '''This class represent a SG90 module'''
+	'''This class represent a SG90 module'''
 	frequency=50 #Impulse frequency(Hz)
 	delta_theta=0.2 #Rotation interval(degree)
 	min_delay=0.0006 #The theoretical time to rotate delta_theta(s)
@@ -19,12 +19,12 @@ class Rotation:
 			self.max_theta=180
 		else:
 			self.max_theta=max_theta
-                if(init_theta<min_theta or init_theta>max_theta):
-                    self.init_theta=(self.min_theta+self.max_theta)/2
-                else:
-		    self.init_theta=init_theta #Initial theta
-                self.min_dutycycle=2.5+self.min_theta*10/180
-                self.max_dutycycle=2.5+self.max_theta*10/180
+			if(init_theta<min_theta or init_theta>max_theta):
+				self.init_theta=(self.min_theta+self.max_theta)/2
+			else:
+				self.init_theta=init_theta #Initial theta
+				self.min_dutycycle=2.5+self.min_theta*10/180
+				self.max_dutycycle=2.5+self.max_theta*10/180
 		
 		
 	def setup(self):
@@ -63,7 +63,7 @@ class Rotation:
 		'''
 		Rotate to specify theta
 		'''
-                if(theta<0 or theta>180):
+		if(theta<0 or theta>180):
 			return
 		self.dutycycle=2.5+theta*10/180
 		self.pwm.ChangeDutyCycle(self.dutycycle)
